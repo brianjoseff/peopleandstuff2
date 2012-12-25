2a4be7dcee8a9326d2e2bcb5d5844c8807f0bc7d
B Joseff <brianjoseff@Brians-MacBook-Air.local>
2012-12-24 18:20:43 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peopleandstuff::Application.config.secret_token = '99458456fe479a1104f53bce1fbfcb267f3ee7c96e6d8971a444a2c02208450bb5cddc13c9ea06799cbc4aea33d6db68f832278de4383b7fd30be084398b2104'
